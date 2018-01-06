584177528add93811bfc429e7f598e509c84eb98
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-06 03:42:36 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,7 +12,7 @@ class SongsController < ApplicationController
 
   get 'songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    erb :'songs/show'
+    erb :'/songs/show'
   end
 
   post '/songs' do
@@ -26,6 +26,7 @@ class SongsController < ApplicationController
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
   end
 
   patch '/songs/:slug' do

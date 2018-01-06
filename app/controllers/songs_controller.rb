4a0f7d8e36b95e8a4b381a1f3ac7a645fe7aa6ce
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-06 03:44:33 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,7 +3,7 @@ class SongsController < ApplicationController
   use Rack::Flash
   get '/songs' do
     @songs = Song.all
-    erb :'/songs/index'
+    erb :'songs/index'
   end
 
   get '/songs/new' do

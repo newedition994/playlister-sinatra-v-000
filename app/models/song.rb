a6d0a8f656e2067d33fe11d3df72ccb613ff662f
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-06 02:43:23 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,8 @@ class Song < ActiveRecord::Base
   belongs_to :artists
   has_many :song_genres
   has_many :genres, :through => :song_genres
+
+  def slug
+    name.downcase.gsub(" ","-")
+  end
 end

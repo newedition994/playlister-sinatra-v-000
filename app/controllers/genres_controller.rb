5ff1469449bb0e452c7de6149320d0b695b90b18
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-06 03:42:57 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,12 +2,12 @@ require 'rack-flash'
 class GenresController < ApplicationController
   get '/genres' do
     @genres = Genre.all
-    erb :'genres/index'
+    erb :'/genres/index'
   end
 
   get '/genres/:slug' do
     @genre = genre.find_by_slug(params[:slug])
-    erb :'genre/show'
+    erb :'/genre/show'
   end
-  
+
 end

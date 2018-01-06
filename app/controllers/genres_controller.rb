f8c206cecb8b1dbdb4a44defcaca2f33412ff212
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-06 02:37:20 +0000

    automatically backed up by learn

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,13 @@
+require 'rack-flash'
+class GenresController < ApplicationController
+  get '/genres' do
+    @genres = Genre.all
+    erb :'genres/index'
+  end
+
+  get '/genres/:slug' do
+    @genre = genre.find_by_slug(params[:slug])
+    erb :'genre/show'
+  end
+  
+end
